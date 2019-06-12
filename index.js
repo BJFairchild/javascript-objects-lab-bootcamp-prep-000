0c4faa1e23cec5ceff6869faffdb998bd4378eaa
Brittan Fairchild <brittanfairchild@gmail.com>
2019-06-12 22:21:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,5 +12,6 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-  return Object.assign({}, object, {[key]:()})
+  var newObj = Object.assign({}, object)
+  
 }
